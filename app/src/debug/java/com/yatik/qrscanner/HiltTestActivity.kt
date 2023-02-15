19e3d8ffda83663b280b05f6f77a6fb7cb8a90d1
Yatik <yatiksihag01@gmail.com>
2023-02-15 22:34:22 +0530

    testing

---
+++ app/src/debug/java/com/yatik/qrscanner/HiltTestActivity.kt
@@ -0,0 +1,7 @@
+package com.yatik.qrscanner
+
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HiltTestActivity : AppCompatActivity()
---
+++ app/src/main/java/com/yatik/qrscanner/di/TestAppModule.kt
@@ -0,0 +1,25 @@
+package com.yatik.qrscanner.di
+
+import android.content.Context
+import androidx.room.Room
+import com.yatik.qrscanner.database.BarcodeRoomDataBase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Named
+
+@Module
+@InstallIn(SingletonComponent::class)
+object TestAppModule {
+
+    @Provides
+    @Named("test_db")
+    // New instance for every test, not singleton
+    fun providesInMemoryDb(@ApplicationContext context: Context) =
+        Room.inMemoryDatabaseBuilder(context, BarcodeRoomDataBase::class.java)
+            .allowMainThreadQueries()
+            .build()
+
+}
